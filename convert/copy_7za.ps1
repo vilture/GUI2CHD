a9554999fbc22665fa261c458740167a24e2f903
Vilture <31757745+vilture@users.noreply.github.com>
2025-06-14 18:32:06 +0700

    Delete convert/copy_7za.ps1

--- convert/copy_7za.ps1
+++
@@ -1,7 +0,0 @@
-$targetDir = "bin\Debug\net6.0-windows\7za"
-if (-not (Test-Path $targetDir)) {
-    New-Item -ItemType Directory -Path $targetDir -Force
-}
-
-Copy-Item "7za\*" -Destination $targetDir -Force
-Write-Host "Файлы 7za скопированы в $targetDir" 
